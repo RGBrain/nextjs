79b0f272391267dbda8be8064b6f9a0f9dbd6773
RGBrain <31857067+RGBrain@users.noreply.github.com>
2024-05-28 00:03:37 +0100

    chapter 10 complete

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-      'use client' 
-      import { useState } from 'react';
+      import LikeButton from "./like-button";
+
 
       function Header({ title }) {
         return <h1>{title ? title : 'Default Title'}</h1>;
@@ -8,22 +8,16 @@
       export default function HomePage() {
         const names = ['Georgia', 'Jacob', 'Sienna'];
 
-        const [likes, setLikes] = useState(0);
-
-        function handleClick() {
-          setLikes(likes + 1);
-        }
-
         return (
           <div>
             <Header title="Text passed in as props" />
-            <Header title="Same component reused with different props" />
+            <Header title="Different text passed as props" />
             <ul>
               {names.map((name) => (
                 <li key={name}>{name}</li>
               ))}
             </ul>
-            <button onClick={handleClick}>Like ({likes})</button>
+            < LikeButton />
         </div>
         );
       }
