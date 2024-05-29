3fb0ec1743bfd4abaa66e321f554258d9b9e06e6
RGBrain <31857067+RGBrain@users.noreply.github.com>
2024-05-30 00:25:57 +0100

    next js chapter 3 complete

---
+++ nexyjs-full/nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,9 @@
+// this file is to keep the fonts that will be used throughout the application.
+
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ 
+    subsets: ['latin'], 
+    weight: '400',
+});
