7a32f018653f8caf2f46b232123ca5f0b558864d
RGBrain <31857067+RGBrain@users.noreply.github.com>
2024-05-30 02:37:22 +0100

    nextjs ch 5 complete

--- nexyjs-full/nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nexyjs-full/nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -1,8 +1,13 @@
+'use client'
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -16,20 +21,28 @@ const links = [
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
+
 export default function NavLinks() {
+  const pathname = usePathname();
+  // console.log(pathname);
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          // Link is very similar to using an <a> tag, but has performance benefits.
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx("flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+            // clsx allows conditional rendering, here adding a light blue bg and blue text to current page logo on the nav UI
+            {'bg-sky-100 text-blue-600': pathname === link.href,
+            },
+          )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
